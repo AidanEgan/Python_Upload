ef82691407932bbf6ba17fa5c102cf1b1971f45b
AidanEgan <aidanegan19@gmail.com>
2016-05-08 00:09:38 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#Import all of the things necessary for the project
 import os
 import re
 from flask import Flask, request, url_for, render_template
@@ -5,21 +6,25 @@ from werkzeug import secure_filename
 from os import listdir
 import sqlite3
 
+#Create the database if it is not already created
 if not os.path.exists("database.db"):
     conn = sqlite3.connect('database.db')
-
-    # "DROP TABLE" will get rid of a table, "CREATE TABLE" will make one
+    #Create the two tables needed for the project
+    #"DROP TABLE" will get rid of a table, "CREATE TABLE" will make one
     conn.execute('CREATE TABLE images (FileName TEXT, name TEXT)')
     conn.execute('CREATE TABLE documents (FileName TEXT, name TEXT)')
     conn.close()
 
+#List the allowed extensions, and tell where the files will be uploaded
 UPLOAD_FOLDER = './static'
 IMAGE_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
-DOCUMENT_EXTENSIONS = set(['txt', 'pdf'])
+DOCUMENT_EXTENSIONS = set(['txt', 'pdf', 'docx'])
 
+#Intitialize the flask application
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+#Check whether a given file can be saved or not, and if it's an image or document
 def check_file(filename):
     if '.' in filename and filename.rsplit('.', 1)[1].lower() in IMAGE_EXTENSIONS:
         return "image"
@@ -28,10 +33,17 @@ def check_file(filename):
     else:
         return "none"
 
+#Rename the file so there are no duplicates. This goes through 3 steps to rename the file
+#Step 1: If there is no file that is the default filename, then save it as the default
+#Step 2: If the default name is taken, save the file with the name the user inputted
+#Step 3: If niether of those can be done, put a '()' at the end of the default filename,
+#Step 3 (cont.) with a unique number inside
 def fileRename(fname, folder, name):
     number = 0
+    #Get the main parts of the file name (if fname='file.txt', then main='file' and ext='txt')
     main = fname.rsplit('.', 1)[0]
     ext = fname.rsplit('.', 1)[1]
+    #Do the renaming thing explained above
     for x in folder:
         if fname == x:
             fname = secure_filename(name + '.' + ext)
@@ -40,8 +52,10 @@ def fileRename(fname, folder, name):
                     #Start original function
                     for x in folder:
                         if x.startswith(main):
+                            #Find the '()' part of the relevant filename
                             regx = re.search(r'\(\d+\).' + ext, x)
                             if regx:
+                                #Find the number inside of the '()' part
                                 regx = int(str(re.search(r'(\d+)', regx.group()).group()))
                                 regx = regx + 1
                                 if regx > number:
@@ -49,8 +63,10 @@ def fileRename(fname, folder, name):
                     fname = main + "(%i)" % number + '.' + ext
                     break
             break
+    #Return a unique filname
     return fname
 
+#This adds a file to the database once it has been uploaded
 def addToDb(fname, name, database):
     try:
         FileName = fname
@@ -59,58 +75,65 @@ def addToDb(fname, name, database):
             cur = con.cursor()
             cur.execute("INSERT INTO " + database + " (FileName, name) VALUES (?,?)",(FileName, name) )
             con.commit()
-            result = "Success!"
-            return result
 
     except:
         with sqlite3.connect("database.db") as con:
             con.rollback()
-        result = "error in insert operation"
-        return result
 
-    return result
 
+#This makes sure there are no extraneous files in the documents or images folders
 def syncFileDb(table):
-    toSearch = []
     with sqlite3.connect("database.db") as con:
         cur = con.cursor()
         cur.execute("SELECT FileName FROM " + table)
-        toSearch = cur.fetchall()
-        for x in toSearch:
+        for x in cur.fetchall():
             if not os.path.exists("./static/" + table + "/" + x[0]):
                 cur.execute("DELETE FROM " + table + " WHERE FileName = " + '"%s"' % x[0])
 
+#This tells the program what to do when someone connects to the site's main page
 @app.route('/')
 def index():
     syncFileDb("images")
     syncFileDb("documents")
+    #This tells it where the relevant html file is
     return render_template('index.html')
 
-@app.route('/images')
+#This is the python code for the page that shows all uploaded images
+@app.route('/images',methods=['GET', 'POST'])
 def images():
+    order = ""
     con = sqlite3.connect("database.db")
     con.row_factory = sqlite3.Row
-
     cur = con.cursor()
     cur.execute("select * from images order by rowid desc")
+    if request.method == 'POST':
+        order = request.form['order']
+        if len(order) < 5:
+            cur.execute("select * from images order by rowid " + order)
+    rows = cur.fetchall()
+    return render_template("images.html",rows = rows, order=order)
 
-    rows = cur.fetchall();
-    return render_template("images.html",rows = rows)
 
-@app.route('/documents')
+#This is the python code for the page that shows all uploaded documents
+@app.route('/documents',methods=['GET', 'POST'])
 def documents():
+    order = ""
     con = sqlite3.connect("database.db")
     con.row_factory = sqlite3.Row
-
     cur = con.cursor()
     cur.execute("select * from documents order by rowid desc")
+    if request.method == 'POST':
+        order = request.form['order']
+        if len(order) < 5:
+            cur.execute("select * from documents order by rowid " + order)
+    rows = cur.fetchall()
+    return render_template("documents.html",rows = rows, order=order)
 
-    rows = cur.fetchall();
-    return render_template("documents.html",rows = rows)
-
+#This is the python code for the page that lets one upload a file to the server
 @app.route('/upload', methods=['GET', 'POST'])
 def upload():
     error = None
+    #This tells where the 'images' and 'documents' folders are located
     imupfol = listdir(app.config['UPLOAD_FOLDER'] + '/images/')
     docupfol = listdir(app.config['UPLOAD_FOLDER'] + '/documents/')
     if request.method == 'POST':
@@ -118,25 +141,29 @@ def upload():
         file = request.files['file']
         #The name of the file being uploaded (to be used later)
         name = request.form['name']
+        #This makes sure is's a legit file and it has a name
         if file and name !="":
-            # It's a legit file and name
+            # This checks to see if the uploaded file was an image
             if check_file(file.filename) == "image":
-                # This means that the uploaded file was an image
                 filename = fileRename(secure_filename(file.filename), imupfol, name)
                 addToDb(filename, name, "images")
                 file.save(os.path.join(app.config['UPLOAD_FOLDER'], "images", filename))
+            # This checks to see if the uploaded file was a document
             elif check_file(file.filename) == "document":
-                # This means that the uploaded file was a document
                 filename = fileRename(secure_filename(file.filename), docupfol, name)
                 addToDb(filename, name, "documents")
                 file.save(os.path.join(app.config['UPLOAD_FOLDER'], "documents", filename))
             else:
+                #This error is thrown if for whatever reason it is neither an image nor a document
                 error = "Unacceptable File Type"
         else:
+            #This error is thrown when the file isn't good or there is no name given for the file
             error = "Please Fill In All of the Required Info"
+    #This is around the 'return' function because the webpage requires a connection to the database
     with sqlite3.connect("database.db") as con:
         return render_template('upload.html', error=error)
         con.close()
 
+#This is the part that tells the server to start, and tells what port and ip to use
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=80)
